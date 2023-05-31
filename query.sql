bacb95e891d4f44e2fb351faf2b3eb952da5f05e
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-05-31 14:36:57 +0700

    Adding Purchase Page

--- query.sql
+++ query.sql
@@ -23,4 +23,83 @@ CREATE TABLE suppliers(
     name VARCHAR(100) NOT NULL,
     address TEXT NOT NULL,
     phone VARCHAR(20) NOT NULL
-)
+);
+
+CREATE TABLE purchases(
+    invoice VARCHAR(20) PRIMARY KEY,
+    time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    totalsum NUMERIC(19,2) NOT NULL DEFAULT 0,
+    supplier INTEGER,
+    operator INTEGER,
+    FOREIGN KEY(supplier) REFERENCES suppliers(supplierid),
+    FOREIGN KEY(operator) REFERENCES users(userid)
+);
+
+CREATE TABLE purchaseitems(
+    id SERIAL PRIMARY KEY,
+    invoice VARCHAR(20) NOT NULL,
+    itemcode VARCHAR(20) NOT NULL,
+    quantity INTEGER NOT NULL,
+    purchaseprice NUMERIC(19,2) NOT NULL DEFAULT 0,
+    totalprice NUMERIC(19,2) NOT NULL DEFAULT 0,
+    FOREIGN KEY(invoice) REFERENCES purchases(invoice),
+    FOREIGN KEY(itemcode) REFERENCES goods(barcode)
+);
+
+-- Trigger function for purchases
+CREATE OR REPLACE FUNCTION updatepurchase() RETURNS TRIGGER AS $setpurchase$
+    DECLARE
+    oldstock INTEGER;
+    summary NUMERIC;
+    currentinvoice TEXT;
+    BEGIN
+        SELECT stock INTO oldstock FROM goods WHERE barcode = NEW.itemcode OR barcode = OLD.itemcode;
+        IF (TG_OP = 'INSERT') THEN
+            UPDATE goods SET stock = oldstock + NEW.quantity WHERE barcode = NEW.itemcode;
+        currentinvoice := NEW.invoice;
+        ELSIF (TG_OP = 'UPDATE') THEN
+            UPDATE goods SET stock = oldstock - OLD.quantity + NEW.quantity WHERE barcode = NEW.itemcode;
+            currentinvoice := OLD.invoice;
+        ELSIF (TG_OP = 'DELETE') THEN
+            UPDATE goods SET stock = oldstock - OLD.quantity WHERE barcode = OLD.itemcode;
+            currentinvoice := OLD.invoice;
+        END IF;
+        -- Update purchase
+        SELECT sum(totalprice) INTO summary FROM purchaseitems WHERE invoice = currentinvoice;
+        UPDATE purchase SET totalsum = COALESCE(summary,0) WHERE invoice = currentinvoice;
+
+        RETURN NULL;
+    END;
+$setpurchase$ LANGUAGE plpgsql;
+CREATE TRIGGER setpurchase
+AFTER INSERT OR UPDATE OR DELETE ON purchaseitems
+    FOR EACH ROW EXECUTE FUNCTION updatepurchase();
+
+-- Update total price purchase
+CREATE OR REPLACE FUNCTION updatepricepurchase() RETURNS TRIGGER AS $set_totalpricepurchase$
+    DECLARE 
+        price NUMERIC;
+    BEGIN
+        SELECT purchaseprice INTO price FROM goods WHERE barcode = NEW.itemcode;
+        NEW.purchaseprice := price;
+        NEW.totalprice := NEW.quantity * price;
+        RETURN NEW;
+    END;
+$set_totalpricepurchase$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_totalpricepurchase
+BEFORE INSERT OR UPDATE ON purchaseitems
+    FOR EACH ROW EXECUTE FUNCTION updatepricepurchase();
+
+
+-- Generate Invoice
+CREATE OR REPLACE FUNCTION purchaseinvoice() RETURNS text AS $$
+    BEGIN 
+        IF EXISTS (SELECT FROM purchases WHERE invoice = 'INV-' || to_char(current_date, 'YYYYMMDD') || '-1') THEN
+        return 'INV-' || to_char(current_date, 'YYYYMMDD') || '-' || nextval('purchases_invoice_seq');
+        ELSE
+            ALTER SEQUENCE purchases_invoice_seq RESTART WITH 1;
+            return 'INV-' || to_char(current_date, 'YYYYMMDD') || '-' || nextval('purchases_invoice_seq');
+        END IF;
+    END;
+$$ LANGUAGE plpgsql;
--- routes/purchases.js
+++ routes/purchases.js
@@ -1,30 +1,62 @@
 var express = require('express');
-var router  = express.Router();
+var router = express.Router();
 
 module.exports = (pool) => {
 
-    router.get('/', (req,res,next)=>{
+    router.get('/', async (req, res, next) => {
+        try {
+            const sql = `SELECT * FROM purchases`
+            const data = await pool.query(sql)
+            res.render('purchases/index', { title: 'POS - Purchases', current: 'purchases', user: req.session.user, data: data.rows })
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ error: 'Error Getting Data Purchases' })
+        }
+    })
+    router.get('/create', async (req, res, next) => {
+        try {
+            const {userid}  = req.session.user
+            // console.log({user : user.userid})
+            console.log(userid)
+            const sql = `INSERT INTO purchases(invoice, totalsum, operator) VALUES(purchaseinvoice(),0,$1) returning *`
+            const data = await pool.query(sql, [userid])
+            res.redirect(`/purchases/show/${data.rows[0].invoice}`)
+        } catch (error) {
+            console.log(error.message)
+            res.status(500).json({ error: 'Error Creating Data Purchases' })
+        }
+    })
+
+    router.get('/show/:invoice', async (req, res, next) => {
+        try {
+            const data = await pool.query('SELECT * FROM purchases WHERE invoice = $1', [req.params.invoice])
+            console.log(data)
+            res.render('purchases/form', { title: 'POS - Add', current: 'purchases', user: req.session.user, data: data, cardheader: 'TRANSACTION' })
+        } catch (error) {
+            console.log(error.message)
+            res.status(500).json({ error: 'Error Showing Data Purchases' })
+        }
+    })
+
+    router.post('/show/:invoice', (req, res, next) => {
 
     })
 
-    router.get('/add', (req,res,next)=>{
-        
+
+    router.post('/add', (req, res, next) => {
+
     })
 
-    router.post('/add', (req,res,next)=>{
-        
+    router.get('/edit', async (req, res, next) => {
+
     })
 
-    router.get('/edit', (req,res,next)=>{
-        
+    router.post('/edit', (req, res, next) => {
+
     })
 
-    router.post('/edit', (req,res,next)=>{
-        
-    })
+    router.get('/delete', (req, res, next) => {
 
-    router.get('/delete', (req,res,next)=>{
-        
     })
     return router
 }
