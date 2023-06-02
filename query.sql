ad40f8f3056f9dd14189161c49d7cc892150a2f2
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-06-02 15:21:53 +0700

    Sales Page maybe done (?)

--- query.sql
+++ query.sql
@@ -55,6 +55,32 @@ CREATE TABLE purchaseitems(
     FOREIGN KEY(itemcode) REFERENCES goods(barcode)
 );
 
+CREATE TABLE sales (
+    invoice VARCHAR(20) PRIMARY KEY NOT NULL,
+    time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    totalsum NUMERIC(19,2),
+    pay NUMERIC(19,2),
+    change NUMERIC(19,2),
+    customer INTEGER,
+    operator INTEGER,
+    FOREIGN KEY(customer) REFERENCES customers(customerid),
+    FOREIGN KEY(operator) REFERENCES users(userid)
+);
+
+CREATE TABLE saleitems(
+    id SERIAL PRIMARY KEY,
+    invoice VARCHAR(20) NOT NULL,
+    itemcode VARCHAR(20) NOT NULL,
+    quantity INTEGER NOT NULL,
+    sellingprice NUMERIC(19,2) NOT NULL DEFAULT 0,
+    totalprice NUMERIC(19,2) NOT NULL DEFAULT 0,
+    FOREIGN KEY(invoice) REFERENCES sales(invoice) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+        NOT VALID,
+    FOREIGN KEY(itemcode) REFERENCES goods(barcode)
+)
+
 -- Trigger function for purchases
 CREATE OR REPLACE FUNCTION updatepurchase() RETURNS TRIGGER AS $setpurchase$
     DECLARE
@@ -101,7 +127,7 @@ BEFORE INSERT OR UPDATE ON purchaseitems
     FOR EACH ROW EXECUTE FUNCTION updatepricepurchase();
 
 
--- Generate Invoice
+-- Generate Invoice Purchases
 CREATE OR REPLACE FUNCTION purchaseinvoice() RETURNS text AS $$
     BEGIN 
         IF EXISTS (SELECT FROM purchases WHERE invoice = 'INV-' || to_char(current_date, 'YYYYMMDD') || '-1') THEN
@@ -112,3 +138,61 @@ CREATE OR REPLACE FUNCTION purchaseinvoice() RETURNS text AS $$
         END IF;
     END;
 $$ LANGUAGE plpgsql;
+
+-- Generate Invoice Sales
+CREATE OR REPLACE FUNCTION sales() RETURNS text AS $$
+    BEGIN
+        IF EXISTS (SELECT FROM sales WHERE invoice = 'INV-PENJ' || to_char(current_date, 'YYYYMMDD') || '-1') THEN
+            return 'INV-PENJ' || to_char(current_date, 'YYYYMMDD') || '-' || nextval('sales_invoice_seq');
+        ELSE
+            ALTER SEQUENCE sales_invoice_seq RESTART WITH 1;
+            return 'INV-PENJ' || to_char(current_date, 'YYYYMMDD') || '-' || nextval('sales_invoice_seq');
+        END IF;
+    END;
+$$ LANGUAGE plpgsql;
+
+-- Trigger Function For Sales
+CREATE OR REPLACE FUNCTION updatesale() RETURNS TRIGGER AS $setsale$
+    DECLARE
+    oldstock INTEGER;
+    summary NUMERIC;
+    currentinvoice TEXT;
+    BEGIN 
+        SELECT stock INTO oldstock FROM goods WHERE barcode = NEW.itemcode OR barcode = OLD.itemcode;
+        IF(TG_OP = 'INSERT') THEN
+            UPDATE goods SET stock = oldstock - NEW.quantity WHERE barcode = NEW.itemcode;
+            currentinvoice := NEW.invoice;
+        ELSIF (TG_OP = 'UPDATE') THEN
+            UPDATE goods SET stock = oldstock + OLD.quantity - NEW.quantity WHERE barcode = NEW.itemcode;
+            currentinvoice := OLD.invoice;
+        ELSIF (TG_OP = 'DELETE') THEN
+            UPDATE goods SET stock = oldstock + OLD.quantity WHERE barcode = OLD.itemcode;
+            currentinvoice := OLD.invoice;
+        END IF;
+        -- update sales
+        SELECT sum(totalprice) INTO summary FROM saleitems WHERE invoice = currentinvoice;
+        UPDATE sales SET totalsum = COALESCE(summary, 0) WHERE invoice = currentinvoice;
+
+        RETURN NULL;
+    END;
+$setsale$ LANGUAGE plpgsql;
+
+CREATE TRIGGER setsale
+AFTER INSERT OR UPDATE OR DELETE ON saleitems
+    FOR EACH ROW EXECUTE FUNCTION updatesale();
+
+-- update totalprice sale
+CREATE OR REPLACE FUNCTION updatepricesale() RETURNS TRIGGER AS $set_totalpricesale$
+    DECLARE
+        sellprice NUMERIC;
+    BEGIN
+        SELECT sellingprice INTO sellprice FROM goods WHERE barcode = NEW.itemcode;
+        NEW.sellingprice := sellprice;
+        NEW.totalprice := NEW.quantity * sellprice;
+        RETURN NEW;
+    END;
+$set_totalpricesale$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_totalpricesale
+BEFORE INSERT OR UPDATE ON saleitems
+    FOR EACH ROW EXECUTE FUNCTION updatepricesale();
--- routes/sales.js
+++ routes/sales.js
@@ -1,7 +1,131 @@
 var express = require('express')
 var router = express.Router()
+var moment = require('moment')
 
-module.exports = (pool)=>{ 
+module.exports = (pool) => {
+    router.get('/', async (req, res, next) => {
+        try {
+            const sql = `SELECT * FROM sales LEFT JOIN customers ON sales.customer = customers.customerid `
+            const data = await pool.query(sql)
+            res.render('sales/index', { title: 'POS - Sales', current: 'sales', user: req.session.user, data: data.rows, moment })
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ error: 'Error Getting Data Sales' })
+        }
+    })
 
+    router.get('/create', async (req, res, next) => {
+        try {
+            const { userid } = req.session.user
+            const sql = `INSERT INTO sales(invoice,totalsum,operator) VALUES (sales(),0,$1) returning *`
+            const data = await pool.query(sql, [userid])
+            res.redirect(`/sales/show/${data.rows[0].invoice}`)
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ erro: 'Error Creating Data Sales' })
+        }
+    })
+
+    router.get('/show/:invoice', async (req, res, next) => {
+        try {
+            const { invoice } = req.params
+            const invoicesql = `SELECT * FROM sales WHERE invoice = $1`
+            const goodsSql = 'SELECT * FROM goods'
+            const custSql = 'SELECT * FROM customers'
+            const getInvoice = await pool.query(invoicesql, [invoice])
+            const getBarcode = await pool.query(goodsSql)
+            const getCustomer = await pool.query(custSql)
+            res.render('sales/form', {
+                title: 'POS - Show',
+                current: 'sales',
+                user: req.session.user,
+                moment,
+                cardheader: 'TRANSACTION',
+                data: getInvoice.rows[0],
+                barcode: getBarcode.rows,
+                customer: getCustomer.rows
+            })
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ error: 'Error Showing Data Sales' })
+        }
+    })
+
+    router.post('/show/:invoice', async (req, res, next) => {
+        const { invoice } = req.params
+        const { totalsum, pay, change, customername } = req.body
+        const {userid} = req.session.user
+        const sql = `UPDATE sales SET totalsum = $1, pay = $2, change = $3, customer = $4 , operator = $5 WHERE invoice = $6`
+        await pool.query(sql, [totalsum, pay, change, customername, userid,invoice])
+        console.log('Success Creating Data Sales')
+        res.redirect('/sales')
+    })
+
+    router.get('/goods/:barcode', async (req, res, next) => {
+        try {
+            const { barcode } = req.params
+            const sql = `SELECT * FROM goods WHERE barcode = $1`
+            const data = await pool.query(sql, [barcode])
+            console.log('Showing Data Barcode Success')
+            res.json(data.rows[0])
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ error: 'Error Getting Data Goods' })
+        }
+    })
+
+    router.get('/tables/:invoice', async (req, res, next) => {
+        try {
+            const { invoice } = req.params
+            const sql = `SELECT saleitems.*, goods.name FROM saleitems LEFT JOIN goods ON saleitems.itemcode = goods.barcode WHERE saleitems.invoice = $1 ORDER BY saleitems.id`
+            const data = await pool.query(sql, [invoice])
+            console.log('Showing Table Sale Items Success')
+            // console.log(data.rows)
+            res.json(data.rows)
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ error: 'Error Getting Data Sale Items' })
+        }
+    })
+
+    router.post('/additems', async (req, res, next) => {
+        try {
+            const { invoice, itemcode, quantity } = req.body
+            const sqlSaleItem = `INSERT INTO saleitems(invoice, itemcode, quantity) VALUES($1,$2,$3)`
+            const sqlSale = `SELECT * FROM sales WHERE invoice = $1`
+            await pool.query(sqlSaleItem, [invoice, itemcode, quantity])
+            const data = await pool.query(sqlSale, [invoice])
+            res.json(data.rows[0])
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ error: 'Error Adding Data Sale items' })
+        }
+    })
+
+    router.get('/deleteitems/:id', async (req, res, next) => {
+        try {
+            const { id } = req.params
+            const sql = `DELETE FROM saleitems WHERE id = $1 returning *`
+            const data = await pool.query(sql, [id])
+            res.redirect(`/sales/show/${data.rows[0].invoice}`)
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ error: 'Error Deleting Sale Items' })
+        }
+    })
+
+
+    router.get('/delete/:invoice', async (req, res, next) => {
+        try {
+            const { invoice } = req.params
+            const sql = `DELETE FROM sales WHERE invoice = $1`
+            await pool.query(sql, [invoice])
+            console.log('Delete Data Sales Success')
+            res.redirect('/sales')
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ error: 'Error Deleting Data Sales' })
+        }
+    })
     return router
 }
