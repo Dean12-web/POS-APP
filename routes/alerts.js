70652b5825b8390941f085bc31a6c3518b35af4a
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-06-19 10:22:18 +0700

    Some Edit

--- routes/alerts.js
+++ routes/alerts.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-module.exports = (pool) =>{
+module.exports = (pool) => {
     // Jaascripts Set Interval
+    router.get('/', async (req, res, next) => {
+        const {rows: data} = await pool.query('SELECT * FROM goods');
+        res.json({ data: data })
+    })
     return router
 }
