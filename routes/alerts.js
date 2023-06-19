65b73ec6ebe949d017e00133cae17505514e717f
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-06-20 06:18:01 +0700

    Challenge 34 done(?)

--- routes/alerts.js
+++ routes/alerts.js
@@ -2,10 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = (pool) => {
-    // Jaascripts Set Interval
     router.get('/', async (req, res, next) => {
-        const {rows: data} = await pool.query('SELECT * FROM goods');
-        res.json({ data: data })
+        const  data  = await pool.query('SELECT * FROM goods WHERE stock <= 5');
+        res.json({ datas: data.rows })
+    })
+
+    router.get('/count', async (req, res, next) => {
+        const {rows : total} = await pool.query('SELECT COUNT(barcode) as total FROM goods WHERE stock <= 5');
+        res.json(total)
     })
     return router
 }
